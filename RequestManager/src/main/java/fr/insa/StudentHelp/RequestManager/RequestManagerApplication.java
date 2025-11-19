5d6a0faee2ea9ef67c514cdf111a5a05901dca78
Sophie Large <sophie.large@wanadaoo.fr>
2025-11-19 15:14:30 +0100

    1st push

--- RequestManager/src/main/java/fr/insa/StudentHelp/RequestManager/RequestManagerApplication.java
+++ RequestManager/src/main/java/fr/insa/StudentHelp/RequestManager/RequestManagerApplication.java
@@ -3,8 +3,8 @@ package fr.insa.StudentHelp.RequestManager;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
 @EnableEurekaServer
